e05a6c97d565f7002affe976ee9fda64ec2701f6
Yehor Nyrkov <>
2024-06-14 23:40:38 +0200

    feat: first quest mechanic

--- Assets/Custom/Scripts/Recipe.cs
+++ Assets/Custom/Scripts/Recipe.cs
@@ -7,4 +7,16 @@ public class Recipe
 {
     public List<Ingredient.Name> ingredients;
     public GameObject resultObjectPrefab;
+    public Category category;
+
+    public Sprite image;
+    public string questDescription = "Quest";
+
+    public enum Category
+    {
+        First,
+        Second,
+        Final
+    }
+
 }
---
+++ Assets/Custom/UI/QuestCardManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(StepManager))]
+public class QuestCardManager : MonoBehaviour
+{
+    public GameObject cardPrefab;
+    public Transform cardParent;
+    public GameObject startButton;
+    public StepManager stepManager;
+
+    private int currentQuestIndex = 0;
+
+
+    public void CreateActiveQuestCard(Recipe recipe)
+    {
+        CreateQuestCard(recipe);
+        stepManager.Next();
+
+        if (startButton != null)
+        {
+            Destroy(startButton);
+        }
+    }
+
+    void CreateQuestCard(Recipe recipe)
+    {
+        GameObject newCard = Instantiate(cardPrefab, cardParent);
+        newCard.name = "Card " + currentQuestIndex;
+
+        // Настройка UI элементов внутри карты
+        Transform modalText = newCard.transform.Find("Modal Text");
+        if (modalText != null)
+        {
+            TextMeshProUGUI textComponent = modalText.GetComponent<TextMeshProUGUI>();
+            if (textComponent != null)
+            {
+                textComponent.text = recipe.questDescription;
+            }
+        }
+
+        Transform imageHolder = newCard.transform.Find("Mask Background/Image Bounds/Image Card");
+        if (imageHolder != null)
+        {
+            Image imageComponent = imageHolder.GetComponent<Image>();
+            // if (imageComponent != null && recipe.image != null)
+            if (imageComponent != null)
+            {
+                imageComponent.sprite = recipe.image;
+            }
+        }
+
+        stepManager.AddStep(newCard, "Step " + (currentQuestIndex + 1));
+
+        currentQuestIndex++;
+    }
+}
